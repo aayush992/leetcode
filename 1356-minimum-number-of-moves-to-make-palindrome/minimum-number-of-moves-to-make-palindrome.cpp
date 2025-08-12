47c870709df4475d8775fb3196eebeed21771a4b
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2025-08-12 12:13:56 +0530

    Time: 9 ms (70.11%) | Memory: 9.6 MB (30.53%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
        int n=s.length();
-       int i=0,j=n-1;
+       int i=0; //left pointer
+       int j=n-1;//right pointer
        int c=0;
        while(i<j){
-        if(s[i]==s[j]){
+        if(s[i]==s[j]){ //if last and first char are equal
             i++;j--;
         }
             else {
-                int k = j;
-                while (k > i && s[k] != s[i]) k--;
-
+                int k = j;//start checking from last charcater
+                while (k > i && s[k] != s[i]) k--;//check till not reaching i
+                            
                 if (k == i) {
                     // Middle char, swap with next
                     swap(s[i], s[i+1]);
                         c++;
                 } else {
-                    // Bubble matched char to r
+                    // Bubble matched char to right i.e. j
                     for (int a = k; a < j; a++) {
                         swap(s[a], s[a+1]);
                         c++;
