b9e6a9ff44784e30a0df3a5fc1cc5f6ab6b6cc8c
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2025-08-23 11:49:38 +0530

    Time: 47 ms (65.81%) | Memory: 15.6 MB (23.38%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     int numSquares(int n) {
-        if(n<=0){
+         if(n<=0){
+            return 0;
+         }
+         vector<int>cnt({0});
+         while(cnt.size()<=n){
+            int m=cnt.size();
+            int cnt1=INT_MAX;
+            for(int i=1;i*i<=m;i++){
+                cnt1=min(cnt1,cnt[m-i*i]+1);
+            }
+            cnt.push_back(cnt1);
+         }
+         return cnt[n];
+     
+     
+     
+     
+      /*  if(n<=0){
             return 0;
         }
         vector<int>cnt(n+1,INT_MAX);
@@ -12,5 +29,6 @@ public:
             }
         }
         return cnt[n];
+        */
     }
 };
