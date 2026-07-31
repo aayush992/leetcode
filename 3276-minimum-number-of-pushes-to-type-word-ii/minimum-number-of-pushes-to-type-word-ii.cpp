e790edcd117dadabcd330d895a443046cfb19152
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2026-07-31 20:13:52 +0530

    Time: 6 ms (75.61%) | Memory: 25.2 MB (96.15%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+         vector<int>map(26,0);
+
+         for(char &ch:word){
+            map[ch-'a']++;
+         }
+         sort(begin(map),end(map),greater<int>());
+            int result=0;
+
+         for(int i=0;i<26;i++){
+           int  freq=map[i];
+            int press=(i/8+1);
+            result+=freq*press;
+         }
+         return result;
+    }
+};
