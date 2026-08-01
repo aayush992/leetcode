aae7cf74a6164eab3ddff31273edb1acbd45ce60
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2026-08-01 20:17:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (69.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-
+    int t[22][22];
     int solve(int i,int j, vector<int>& nums){
         if(i>j){
             return 0;
         }
         if(i==j)return nums[i];
-        
+        if(t[i][j]!=-1)return t[i][j];
+
         int take_i=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
 
         int take_j=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
 
-        return max(take_i,take_j);
+        return t[i][j]= max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
+
+        memset(t,-1,sizeof(t));
+
         int score=accumulate(begin(nums),end(nums),0);
         
         int n=nums.size();
