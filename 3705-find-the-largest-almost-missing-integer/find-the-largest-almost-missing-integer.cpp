71a34e1138cee0d551822d4762b7ae423be999d7
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2026-08-18 13:21:08 +0530

    Time: 9 ms (26.32%) | Memory: 32.8 MB (8.65%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        int n=nums.size();
-        int i=0,j=k-1;
-        
-        unordered_map<int,int>mp;
+        int n = nums.size();
+        int i = 0, j = k - 1;
 
-        while(j<n){
-        int c=0;
-        for(c=i;c<=j;c++){
-            mp[nums[c]]++;
-        }
-        i++;
-        j++;
-        }
-        int ans,maxans=-1;
+        unordered_map<int, int> mp;
 
-        for(auto it:mp){
-            if(it.second==1 || n==k){
-                ans=it.first;
-                maxans=max(ans,maxans);
+        while(j < n) {
+
+            unordered_set<int> seen;
+
+            for(int c = i; c <= j; c++) {
+                if(seen.find(nums[c]) == seen.end()) {
+                    mp[nums[c]]++;
+                    seen.insert(nums[c]);
+                }
+            }
+
+            i++;
+            j++;
+        }
+
+        int maxans = -1;
+
+        for(auto it : mp) {
+            if(it.second == 1) {
+                maxans = max(maxans, it.first);
             }
         }
 
         return maxans;
-
     }
 };
