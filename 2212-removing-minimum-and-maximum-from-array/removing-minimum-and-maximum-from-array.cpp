3483c1c4bbd14016046224df37b08f26dec9c9a0
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2026-08-31 18:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 92.2 MB (22.32%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
+        if(n==1 || n==2){
+            return n;
+        }
+        int i=0,j=n-1;
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        int min_idx=0,max_idx=0;
 
-        int minElIdx = min_element(begin(nums), end(nums)) - begin(nums);
-        int maxElidx = max_element(begin(nums), end(nums)) - begin(nums);
+        while(i<=j){
+            if(nums[i]>maxi){
+                maxi=nums[i];
+                max_idx=i;
+            }
+            if(nums[i]<mini){
+                mini=nums[i];
+                min_idx=i;
+            }
+            if(nums[j]>maxi){
+                maxi=nums[j];
+                max_idx=j;
+            }
+            if(nums[j]<mini){
+                mini=nums[j];
+                min_idx=j;
+            }
+            i++;
+            j--;
+        }
+        
+        int left = max(min_idx, max_idx) + 1;
+        int right = n - min(min_idx, max_idx);
+        int both = min(min_idx, max_idx) + 1 + n - max(min_idx, max_idx);
 
-        int left  = min(minElIdx, maxElidx);
-        int right = max(minElIdx, maxElidx);
-
-        return min({left+1+n-right, right+1, n-left});
+        return min({left, right, both});
+        
     }
 };
